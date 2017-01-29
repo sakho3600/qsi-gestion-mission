060446d297261d2037eedd73ce39e7f0dab4ffbe
sylvie eveque <sylvy05@gmail.com>
2017-01-29 10:43:31 +0100

    config qsi-gestion-mission

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :test, Test.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "testtphoenix2017.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "qsi-gestion-mission.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
@@ -23,11 +23,7 @@ config :logger, level: :info
 # Configure your database
 config :test, Test.Repo,
   adapter: Ecto.Adapters.Postgres,
- # url: System.get_env("DATABASE_URL"),
-  hostname=System.get_env("PG_HOST"),
-  username=System.get_env("PG_USERNAME"),
-  password=System.get_env("PG_PASSWORD"),
-  database=System.get_env("PG_DATABASE"),
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
